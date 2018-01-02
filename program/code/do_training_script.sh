739ff9cdf65f518a64eeb2652c287e9e1e367a85
Weirenlan <MIBlue119@users.noreply.github.com>
2018-01-03 01:53:12 +0800

    Update do_training_script.sh

--- program/code/do_training_script.sh
+++ program/code/do_training_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-dir=$藍偉任/program/input
-test_sr=$藍偉任/test_data/EBUSimagestraining_test/test2
-train_sr=$藍偉任/test_data/EBUSimagestraining_test/train2_flip_rotate_mless
-test_d=$藍偉任/program/input/test
-train_d=$藍偉任/program/input/train
-create_lmdb=$藍偉任/program/code/create_lmdb_227.py
+dir=$thesis/program/input
+test_sr=$thesis/test_data/EBUSimagestraining_test/test2
+train_sr=$thesis/test_data/EBUSimagestraining_test/train2_flip_rotate_mless
+test_d=$thesis/program/input/test
+train_d=$thesis/program/input/train
+create_lmdb=$thesis/program/code/create_lmdb_227.py
 
 
 filelist=$(ls ${dir})
@@ -28,7 +28,7 @@ python2 $create_lmdb
 #echo "Hi, I'm sleeping for 3 seconds..."
 #sleep 18
 
-C:/caffe/Build/x64/Release/compute_image_mean.exe -backend=lmdb  $藍偉任/program/input/train_lmdb  $藍偉任/program/input/mean.binaryproto
-$藍偉任/ program/caffe_models/caffenet/solver_2.prototxt --weights $藍偉任/program/pretrained_models/bvlc_reference_caffenet.caffemodel  2>&1 | tee $藍偉任/ program/caffe_models/caffenet/model_2_train_trasfer.log
+C:/caffe/Build/x64/Release/compute_image_mean.exe -backend=lmdb  $thesis/program/input/train_lmdb  $thesis/program/input/mean.binaryproto
+$thesis/ program/caffe_models/caffenet/solver_2.prototxt --weights $thesis/program/pretrained_models/bvlc_reference_caffenet.caffemodel  2>&1 | tee $thesis/ program/caffe_models/caffenet/model_2_train_trasfer.log
 
 read -p "Press any key..."
